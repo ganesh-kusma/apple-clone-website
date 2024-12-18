6fbafa58e1074d176759676dfe7da7e1aeab5ab6
Ganesh Kusma <ganesh@nextsolutions.in>
2024-12-19 00:57:45 +0530

    added new file

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,16 @@
-
-const ModelView = () => {
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from './Lights';
+const ModelView = ({index,groupRef,gsapType,controlRef,setRotationState,item,size}) => {
     return (
-        <div>
-            ModelView
-        </div>
+        <View
+        index={index}
+        id={gsapType}
+        className={`border border-red-500 w-full h-full ${index===2}?:'right-[-100%]':''`}
+        >
+        <ambientLight intensity={0.3}/>
+        <PerspectiveCamera makeDefault position={[0,0,4]}/>
+        <Lights/>
+        </View>
     )
 }
 
